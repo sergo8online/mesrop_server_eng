d1e07085fabadfbf4d9a343fa81bc85b69e7d79d
sergo8online <sergotovmasyan888@gmail.com>
2025-05-25 13:56:18 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import io
+import torch
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import numpy as np
+
+app = Flask(__name__)
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'Файл изображения не найден'}), 400
+    try:
+        file = request.files['image']
+        image_bytes = file.read()
+        image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+        image = image.transpose(method=Image.ROTATE_270)
+
+        pixel_values = processor(images=image, return_tensors="pt").pixel_values
+        generated_ids = model.generate(pixel_values)
+        generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        return jsonify({'result': generated_text})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
