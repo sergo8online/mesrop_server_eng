837e5c0269943580c1846f32e820dfee29a5bc51
sergo8online <sergotovmasyan888@gmail.com>
2025-05-26 01:52:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-import os
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
-
-
-#if __name__ == '__main__':
- #   app.run(host='0.0.0.0', port=10000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
